fa1e3759f65239a1eff8858ca1a09cbe7fc663c4
ponyjackal <pony1125@outlook.com>
2022-02-14 02:06:06 -0800

    config hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  }
 };
